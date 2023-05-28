2f2eb8b8e511dd2d5a077e7064a5f81de7f1e2c9
Arnab Majumder <arnabmajumdar399@gmail.com>
2023-05-28 21:09:43 +0530

    first commit

---
+++ src/Sections/Testimonials/index.js
@@ -0,0 +1,110 @@
+import React, { lazy } from "react";
+import styled from "styled-components";
+
+import  Slider  from "react-slick";
+import "../../../node_modules/slick-carousel/slick/slick.css";
+import "../../../node_modules/slick-carousel/slick/slick-theme.css";
+
+const Card = lazy(() => import("../../components/Card/index"));
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 5rem 0;
+`;
+
+const Title = styled.h1`
+  color: #0a0b10;
+  display: inline-block;
+  font-size: calc(1rem + 1.5vw);
+  margin-top: 1.5rem;
+  position: relative;
+  &::before {
+    content: "";
+    height: 1px;
+    width: 50%;
+    position: absolute;
+    left: 50%;
+    bottom: 0;
+    transform: translate(-50%, 0.5rem);
+    /* or 100px */
+    border-bottom: 2px solid var(--purple);
+  }
+`;
+
+const Carousal = styled.div`
+  width: 50vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  @media only Screen and (max-width: 40em) {
+    width: 90vw;
+    .slick-slider .slick-arrow {
+      display: none;
+    }
+  }
+  .slick-slider .slick-arrow:before {
+    color: #0a0b10;
+    font-size: 1.5rem;
+    @media only Screen and (max-width: 40em) {
+      display: none;
+    }
+  }
+  .slick-slider .slick-dots button:before {
+    color: #0a0b10;
+    font-size: 1.5rem;
+  }
+  .slick-slide.slick-active {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin: 0;
+    padding: 0;
+    margin-bottom: 3rem;
+  }
+`;
+
+const Testimonials = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <Section>
+      <Title>Few good words about us!</Title>
+      <Carousal>
+        <Slider {...settings}>
+          <Card
+            text="The program exceeded my expectations. I really did learn more in 5 days than I ever thought possible from Bryan and the whole team."
+            name="Suhas Kaushik"
+            image="avatar-1"
+          />
+
+          <Card
+            text="
+            The program is very well structured as well as knowledgeable. Highly recommended for people looking to do something out of the box."
+            name="Aarushi Goyal"
+            image="avatar-2"
+          />
+
+          <Card
+            text="This program pushed what I thought possible in short time frame, fostering skills in communication with people from all around the world."
+            name="Max Karous"
+            image="avatar-3"
+          />
+        </Slider>
+      </Carousal>
+    </Section>
+  );
+};
+
+export default Testimonials;
